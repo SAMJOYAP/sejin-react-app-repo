f0607612bd708abb839bfb01389620877139539c
SEJ <jink6111@gmail.com>
2026-01-27 19:47:13 +0900

    chore: test cd

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>3조를 응원해주세요!!</h1>
+      <h1>3조를 응원해주세요</h1>
       <div className='card'>
         <button onClick={() => setCount(count => count + 1)}>지금까지의 응원 {count}</button>
       </div>
