6440a13cf97f109b6ade05151b40c9ef9fb49db4
SEJ <jink6111@gmail.com>
2026-01-29 20:38:37 +0900

    refactor: clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const fetchCheer = async () => {
+      try {
+        const res = await axios.get('http://localhost:3000/api/count');
+        setCount(res.data.count);
+      } catch (error) {
+        console.error('API 호출 실패:', error);
+      }
+    };
+
+    fetchCheer();
+  }, []);
+
+  const handleCheer = async () => {
+    try {
+      const res = await axios.post('http://localhost:3000/api/count');
+      setCount(res.data.count);
+    } catch (error) {
+      console.error('응원 실패:', error);
+    }
+  };
 
   return (
     <>
@@ -16,9 +40,10 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
+
       <h1>3조를 응원해주세요</h1>
       <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>지금까지의 응원 {count}</button>
+        <button onClick={handleCheer}>지금까지의 응원 {count}</button>
       </div>
     </>
   );
